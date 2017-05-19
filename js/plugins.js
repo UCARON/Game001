668360a4ce754f4ab6edb8e7b5e0de3a746ecd4c
UCARON <sontamy@gmail.com>
2017-05-20 00:08:29 +0900

    a

--- js/plugins.js
+++ js/plugins.js
@@ -6,5 +6,6 @@ var $plugins =
 {"name":"RTK_Test","status":true,"description":"テスト用プラグイン","parameters":{"アクター名の後にIDを表示":"1"}},
 {"name":"UR65_SmartPhoneUI","status":true,"description":"スマホ用UI  ver 1.0.0\nUIのサイズをスマートフォン向けに最適化します。","parameters":{"タイトル":"1","メニュー":"1","アイテム":"1","スキル":"1","装備":"1","オプション":"1","ゲーム終了":"1","戦闘":"1","ショップ":"1","イベント関係":"1","アイコン位置修正":"1"}},
 {"name":"PictureCallCommon","status":false,"description":"ピクチャのボタン化プラグイン","parameters":{"透明色を考慮":"ON","ピクチャ番号の変数番号":"0","ポインタX座標の変数番号":"0","ポインタY座標の変数番号":"0","タッチ操作抑制":"OFF","戦闘中常にコモン実行":"OFF"}},
-{"name":"TMEventClick","status":true,"description":"イベントをクリックして起動、ドラッグで移動させる機能を\n追加します。","parameters":{"dropStart":"0","noDropRegions":"","canMultipleDrop":"0","lockTurnEnabled":"0"}}
+{"name":"TMEventClick","status":false,"description":"イベントをクリックして起動、ドラッグで移動させる機能を\n追加します。","parameters":{"dropStart":"0","noDropRegions":"","canMultipleDrop":"0","lockTurnEnabled":"0"}},
+{"name":"EventReSpawn","status":true,"description":"イベント動的生成プラグイン","parameters":{}}
 ];
